7a4bf40dfb2c3e78c23099bc4caabac4efc9f4fe
Torkovwryy <261977737+Torkovwryy@users.noreply.github.com>
2026-04-10 21:16:27 -0300

    refactor(python): support pathlib.Path objects in MemoryMap constructor

--- python/xmap.py
+++ python/xmap.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 import xmap_ext
 
 
 class MemoryMap(xmap_ext.MemoryMap):
+    def __init__(self, filepath, mode=Mode.ReadOnly, flags=Flags.None_):
+        # Converte Pathlib para string antes de enviar para o C++
+        str_path = str(filepath) if isinstance(filepath, Path) else filepath
+        super().__init__(str_path, mode, flags)
+
     """High-level wrapper for Memory-Mapped files with Zero-Copy support."""
     pass
 
